d2d194aad3c6eb25423f12e555cab1cfa1ee0ded
Swapnil <schakravarti930@gmail.com>
2020-11-06 21:39:06 +0530

    [Swapnil] Add : Method to retrieve top 3 rated reviews

---
+++ ProductReviewManagement/Management.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace ProductReviewManagement
+{
+    class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReview in listProductReview
+                                orderby productReview.Rating descending
+                                select productReview).Take(3);
+            foreach(var list in recordedData)
+            {
+                Console.WriteLine(list.ToString());
+            }
+        }
+    }
+}
